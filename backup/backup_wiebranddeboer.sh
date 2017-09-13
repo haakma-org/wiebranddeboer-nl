5ff6385db687a229c1e49e70956920ac04538170
sido <sido@haakma.org>
2017-09-13 22:51:43 +0200

    [FIX] updated www_dir for docker configuration

--- backup/backup_wiebranddeboer.sh
+++ backup/backup_wiebranddeboer.sh
@@ -5,8 +5,9 @@ SQL_SCHEMA=${2}
 
 DATE=`date +%Y-%m-%d`
 BASE_DIR=/home/backup/today
+WWW_DIR=/home/sites
 
 mysqldump -p${SQL_PASSWORD} ${SQL_SCHEMA} > /home/sido/backup/today/${DATE}_wiebranddeboer.sql
 
-tar -cvf /home/sido/backup/today/${DATE}_wiebranddeboer.tar /srv/www/wiebranddeboer.nl/
+tar -cvf /home/sido/backup/today/${DATE}_wiebranddeboer.tar ${WWW_DIR}/wiebranddeboer.nl/
 
