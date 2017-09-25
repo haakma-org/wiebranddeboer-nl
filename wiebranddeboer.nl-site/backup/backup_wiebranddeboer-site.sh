aeff91a48773bd89e1934d02782d55bdc8bd06d2
sido <sido@haakma.org>
2017-09-25 23:29:56 +0200

    [FEATURE] removed one sql-dump

--- wiebranddeboer.nl-site/backup/backup_wiebranddeboer-site.sh
+++ wiebranddeboer.nl-site/backup/backup_wiebranddeboer-site.sh
@@ -12,7 +12,6 @@ WWW_DIR=/home/sites
 
 echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
 mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT} > ${BASE_DIR}/${DATE}_wiebranddeboer.sql
-mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} > ${BASE_DIR}/${DATE}
 
 echo "[INFO] Backup directory : ${WWW_DIR}/wiebranddeboer.nl/"
 tar -cvf ${BASE_DIR/${DATE}_wiebranddeboer.tar ${WWW_DIR}/wiebranddeboer.nl/
